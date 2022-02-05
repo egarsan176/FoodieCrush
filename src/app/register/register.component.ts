111b5cb2bac25fca1e2da777fd5056d1dbe6e409
egarsan176 <estefaniagarci@gmail.com>
2022-02-05 18:57:02 +0100

    Terminadas vistas login y register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+
+  // miFormulario: FormGroup = this.fb.group({
+  //   nombre: ['', [ Validators.required, Validators.pattern( this.validatorService.nombreApellidoPattern ) ] ],
+  //   email: ['', [ Validators.required, Validators.pattern( this.validatorService.emailPattern ) ], [ this.emailValidator ] ],
+  //   username: ['', [ Validators.required, this.validatorService.noPuedeSerStrider ] ],
+  //   password: ['', [ Validators.required, Validators.minLength(6) ]  ],
+  //   password2: ['', [ Validators.required ]  ],
+  // }, {
+  //   validators: [ this.validatorService.camposIguales('password','password2') ]
+  // });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  register(){
+
   }
 
 }
