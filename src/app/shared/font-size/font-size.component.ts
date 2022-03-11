29b580cc6accfee21b5f3a20bff6dc7b2c51e2ac
egarsan176 <estefaniagarci@gmail.com>
2022-03-11 09:49:13 +0100

    finalizado correción accesibilidad

--- src/app/shared/font-size/font-size.component.ts
+++ src/app/shared/font-size/font-size.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-font-size',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FontSIZEComponent implements OnInit {
 
   fontsize = 16;
+  @ViewChild('para', {static: true})
+  para!: ElementRef;
 
   constructor() { }
 
@@ -15,11 +17,19 @@ export class FontSIZEComponent implements OnInit {
   }
 
 
+  //MÉTODO para cambiar el tamaño de la fuente en el documento
   changeFONT(operator: any){
 
     operator === '+' ? this.fontsize++ : this.fontsize--;
     document.getElementsByTagName('body')[0].style.fontSize  = `${this.fontsize}px`;
+    console.log(document.getElementsByTagName('body')[0]);
 
-  }
+  //   document.querySelectorAll('div.size').forEach(
+  //     element => {
+  //       element.style.fontSize = `${this.fontsize}px`;
+  //     }
+  //   )
+    
+   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
    urlBase: 'https://foodiecrush.herokuapp.com'
    //urlBase: 'http://localhost:9000'
-
+  
 };
